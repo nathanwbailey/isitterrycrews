83494a6f54a3a894857a6fb2f32cec5fbfdc6844
Nathan Bailey <nathanbaileyw@gmail.com>
2021-07-25 17:34:02 +0100

    Added Train files

--- train.py
+++ train.py
@@ -1,1 +1,8 @@
-def train(model, num_epochs, )
+import torch
+from early import pytorchtools
+import sys
+
+def train(model, num_epochs, optimizer, loss_function, trainloader, validloader, patience=10, path_to_model = 'model.pth', scheduler=None):
+    print('Training Started')
+    sys.stdout.flush()
+    early_stop = pytorchtools.EarlyStopping(patience=)
